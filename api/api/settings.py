84d2c04b0e7b17436e7476c6840988e5bc25728e
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-02 19:26:02 -0300

    Revert "Force HTTPS usage to heroku app (back-end)"
    This reverts commit 52ebc87ef534d0553eec7c535bdae0ead2c4a182.

--- api/api/settings.py
+++ api/api/settings.py
@@ -58,9 +58,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
