49e42dcf03cc45f878263aead6c93c0b5f44001e
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-03 00:45:24 -0300

    Fix invalid attribution to const type

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 
-const url = "http://localhost:8000";
+let url = "http://localhost:8000";
 if (process.env.NODE_ENV == "production") {
   url = "https://glacial-harbor-86827.herokuapp.com";
 }
@@ -9,8 +9,6 @@ if (process.env.NODE_ENV == "production") {
 const api = axios.create({
   baseURL: url
 });
-
-console.log(api);
 
 Vue.prototype.$axios = api;
 
