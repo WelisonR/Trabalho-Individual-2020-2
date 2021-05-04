876350883c80232a0706c29515091aeaa543b8f6
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-03 22:41:47 -0300

    Lint code according to flake8 settings

--- api/task/serializers.py
+++ api/task/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Task
 
-class TaskSerializer(serializers.ModelSerializer):
 
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Task 
+        model = Task
         fields = ['pk', 'title', 'description', 'created']
